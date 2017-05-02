12e13af6a3b6af57328f1bfabca107a3d4343686
Henry <henryjboles@gmail.com>
2017-05-02 14:44:08 -0700

    Update source.rb

--- source.rb
+++ source.rb
@@ -3,17 +3,72 @@
 #   CENTRAL PROSSESING FILE
 #
 #   CONTAINS:
-#    1.  CONFIG OVERIDE
-#    2.  BOARD CONSTRUCTION
-#    3.  NODE CONSTRUCTION
-#    4.  [CONFIG DECODER]
-#    5.  [FLAG CHECKS]
+#    1.  [FLAG CHECKS]
+#    2.  CONFIG OVERIDE
+#    3.  BOARD CONSTRUCTION
+#    4.  NODE CONSTRUCTION
+#    5.  [CONFIG DECODER]
 #    6.  [OBJECT DEFINITIONS]
 #    7.  [LOOP DEFINITIONS]
 #    8.  [BULIDING]
 #    9. [MAIN LOOP]
 #
 #   [] INDICATES MADATORY SETUP
+
+
+#
+#   FLAG CHECKS
+#
+
+puts Time.now.to_s + ": " + "Loading flag checks..."
+
+def flag(id,chk=false)
+  if chk
+    return ARGV[ARGV.index(id)+1]
+  else
+    return ARGV.include? id
+  end
+end
+
+def fg(id)
+  if ARGV.include? id
+    yield
+  end
+end
+
+def d
+  if flag("-d")
+    print Time.now.to_s + ": "
+    yield
+  end
+end
+
+def e(text)
+  if flag("-d")
+    print Time.now.to_s + ": "
+    puts text
+  end
+end
+
+def ed
+  if flag("-d")
+    print Time.now.to_s + ": "
+    puts "Done!"
+  end
+end
+
+def rc
+  if flag($config["RC-FLAG"]) or $config["RUN-CHECKS"] == "true"
+    print Time.now.to_s + ": "
+    yield
+  end
+end
+
+e "Flag checks loaded."
+
+e "Starting."
+
+e "Loading default configuration..."
 
 $autoParse = <<ENDOFFILE
 
@@ -57,81 +112,64 @@ CHAR-ENEMY: "&"
 [0,0]=>location("TEST",true)
 
 ENDOFFILE
+ed
 
 #
 #   CONFIG DECODER
 #
 
 def resetConfig
+  e "Reseting config..."
   $oldConfig = $config
   $config = {}
+  ed
 end
 
 def parse(config)
+  e "Parsing config..."
   config.split("\n").each do |cache|
-    unless cache.split("")[0] == "#"
+    unless cache.split("")[0] == "#" or cache.strip == ""
       d {print "Scanning config line: ", cache, "\n"}
       cache.scan(/[a-zA-Z-]+: .+/).each do |e|
         d {print "Found match: ", e, "\n"}
+        e "Formatting..."
         es = e.split(/: /)
         es[1].strip!
         es[1].gsub!(/^"|"$/, "")
+        ed
+        e "Adding to config..."
         $config.merge!(Hash[[es]])
+        ed
       end
     else
-      d {print "Found commented out line. Ignoring.", "\n"}
+      d {print "Found blank line. Ignoring.", "\n"}
     end
   end
+  ed
 end
 
-#
-#   FLAG CHECKS
-#
-
-def flag(id,chk=false)
-  if chk
-    return ARGV[ARGV.index(id)+1]
-  else
-    return ARGV.include? id
-  end
-end
-
-def fg(id)
-  if ARGV.include? id
-    yield
-  end
-end
-
-def d
-  if flag("-d")
-    print Time.now.to_s + ": "
-    yield
-  end
-end
-
-def rc
-  if flag($config["RC-FLAG"]) or $config["RUN-CHECKS"] == "true"
-    print Time.now.to_s + ": "
-    yield
-  end
-end
 
 def loadConfig
+  e "Loading config..."
   resetConfig
   parse($autoParse)
   d {
     print "Loaded default configuation: ", $config, "\n"
   }
   if flag($config["MC-FLAG"]) or $config["MANUAL-CONFIG"] == "true"
+    e "Alternate config given. Attempting to open file."
     file = File.open(flag($config["MC-FLAG"],true),"rw")
+    e "File opened."
+    e "Alternate config found. Loading..."
     resetConfig
     parse(file.read)
+    ed
     file.close
+    e "File closed."
     d {
       print "Loaded alternative configuation: ", $config, "\n"
     }
   end
 end
 
-puts ARGV, flag("-d")
 loadConfig
