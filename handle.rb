5958dca9919984c9a8db1ee0d22be1ab9d0793e2
Henry <henryjboles@gmail.com>
2017-05-03 14:41:53 -0700

    Update handle.rb

--- handle.rb
+++ handle.rb
@@ -1,2 +1,19 @@
-#  
-#   HANDLE
+# 
+#   HANDLE.RB
+#   RESPONSE HANDLING FILE
+#
+#   CONTAINS:
+#    1. HANDLING
+#
+#   [] INDICATES MADATORY SETUP
+
+module HANDLE
+  def handle(givenHandlingText)
+    
+    #
+    #   YOUR HANDLING CODE GOES HERE
+    #   REFRENCE SOURCE.RB FOR VALUES AND CLASSES
+    #
+    
+  end
+end
